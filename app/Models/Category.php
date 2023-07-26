6da09b1d3bcaff54bd55ffa47d44efccb1e761e4
axelgolin <axelgolin4@gmail.com>
2023-07-26 11:17:11 -0600

    Cambios de las tablas y los input

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,15 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable=['title','slug'];
+    protected $fillable = ['title','slug'];
 
-    public function posts()
-    {
+
+    public function posts(): BelongsToMany {
         return $this->belongsToMany(Post::class);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Models\User;
 use App\Models\Category;
 
@@ -11,19 +13,18 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable=['title','slug','thumbnail','body','active','published_at','user_id'];
+    protected $fillable = ['title','slug','thumbnail','body','active','published_at','user_id'];
 
+    protected $casts = [
+        'published_at' => 'datetime'
+    ];
 
-    public function user()
-    {
+    public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }
 
-    public function categories()
-    {
+    public function categories(): BelongsToMany {
         return $this->belongsToMany(Category::class);
     }
-
-    
 
 }
