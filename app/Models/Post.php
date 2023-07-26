68fdb54382035c326699677e7bc18e5ad2733546
axelgolin <axelgolin4@gmail.com>
2023-07-26 09:35:08 -0600

    Instalacion de Filament

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable=['title','slug','thumbnail','body','active','published_at','user_id'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    
+
+}
